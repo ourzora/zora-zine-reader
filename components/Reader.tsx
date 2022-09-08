04b0d31c0ac40721263f11e5b148ecd832d44ea1
Dain Blodorn Kim <dain@db13.us>
2022-09-07 17:18:03 -0700

    use speech synth package

--- components/Reader.tsx
+++ components/Reader.tsx
@@ -1,7 +1,7 @@
 import { useEffect } from 'react'
 import useSWR from 'swr'
 import Marquee from "react-fast-marquee"
-import { SpeechSynth } from '@speech-synth'
+import { SpeechSynth } from '@derpyvision/react-speech-synth'
 
 export function Reader({
   slug
